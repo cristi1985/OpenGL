8c4140ac2e7f57fca06f73a140884c242992d04d
Cristi Simion <cristisimion84@yahoo.co.uk>
2019-12-23 13:47:40 +0200

    Tema final

--- Tema/Tema/Tema.cpp
+++ Tema/Tema/Tema.cpp
@@ -1,40 +1,13 @@
 #include <iostream>
 #include <GL/Glut.h>
 
-int meniu_main, submeniu3D, submeniu2D, xrotate, yrotate, zrotate;
-
-void idle();
-
-void init()
-{
-	glPointSize(40.0);
-}
-
-//void display()
-//{
-//	glClear(GL_COLOR_BUFFER_BIT);
-//	glColor3f(0.0, 1.0, 0.0);
-//	glBegin(GL_POLYGON); // initializare desen poligon
-//	glVertex2f(0.0, 0.0);//stabilire coordonate triunghi
-//	glVertex2f(200.0, 200.0);//stabilire coordonate triunghi
-//	glVertex2f(0.0, 200.0);//stabilire coordonate triunghi
-//	glEnd();//sfarsit Desenare
-//
-//	glFlush();//executare functie
-//
-//	glColor3f(1.0, 1.0, 0.0);
-//	glBegin(GL_POINTS);
-//	glVertex2i(0, 0);
-//	glVertex2i(200, 200);
-//	glVertex2i(380, 20);
-//	glEnd();
-//	glFlush();
-//
-//}
+int meniu_main, submeniu3D, submeniu2D;
 
 void displayTriangle()
 {
 	glClear(GL_COLOR_BUFFER_BIT);
+	glMatrixMode(GL_MODELVIEW);
+	glLoadIdentity();
 	glColor3f(0.0, 1.0, 0.0);//stabilire culoare triunghi
 	glBegin(GL_POLYGON); // initializare desen poligon
 	glVertex2f(0.0, 0.0);//stabilire coordonate triunghi
@@ -42,67 +15,60 @@ void displayTriangle()
 	glVertex2f(0.0, 200.0);//stabilire coordonate triunghi
 	glEnd();//sfarsit Desenare
 	glFlush();//executare functie
+	glutSwapBuffers();//schimbare intre buffere 
+	glutPostRedisplay();//reafisare fereastra
 
 }
 
 void displayRectangle()
 {
 	glClear(GL_COLOR_BUFFER_BIT);
+	glMatrixMode(GL_MODELVIEW);
+	glLoadIdentity();
 	glColor3f(1.0, 0.0, 0.0);
-	glBegin(GL_QUADS);
+	glBegin(GL_QUADS);//incepe desenarea
 	glVertex2f(50.0, 0);//conteaza ordinea in care punem coordonatele, pt Quaduri se incepe din stanga jos
 	glVertex2f(100.0, 0);//dreapta jos
 	glVertex2f(100, 50);//stanga sus
 	glVertex2f(50, 50);//dreapta sus
-	glEnd();
-	glFlush();
+	glEnd();//desenarea se termina aici
+	glFlush();//golire buffere si executare comenzi de catre motorul de randare 
+	glutSwapBuffers();
+	glutPostRedisplay();
 }
 
 void displayTorus()
 {
-	glMatrixMode(GL_MODELVIEW);
 	glClear(GL_COLOR_BUFFER_BIT);
+	glMatrixMode(GL_MODELVIEW);
+	glLoadIdentity();
 	glColor3f(0.8, 0.2, 1.0);
 	glLoadIdentity();
 	glPushMatrix();
 	glTranslatef(180, 150, 0);
-	/*xrotate = yrotate = zrotate = 30;
-	xrotate = 33;
-	yrotate = 40;
-	glRotated(xrotate, 1.0, 0.0, 0.0);
-	glRotated(yrotate, 0.0, 1.0, 0.0);*/
-	//glRotated(20, 0.0, 15.0, 1.0);
 	glutWireTorus(100.0, 25.0, 20, 20);
 	glPopMatrix();
-	//glutSwapBuffers();
 	glEnd();
 	glFlush();
-	/*glBegin(GL_POINTS);
-	glFlush();
-	
-	glutIdleFunc(idle)*/;
+	glutSwapBuffers();
 	glutPostRedisplay();
 
 }
 
 void displayTeapot()
 {
-
-	glMatrixMode(GL_MODELVIEW);
 	glClear(GL_COLOR_BUFFER_BIT);
+	glMatrixMode(GL_MODELVIEW);
+	glLoadIdentity();
 	glColor3f(0.8, 0.2, 1.0);
 	glLoadIdentity();
 	glPushMatrix();
-	glTranslatef(180, 150, 0);
-	
-	glutSolidTeapot(20);
+	glTranslatef(180, 150, 0.0f);
+	glutWireTeapot(50);
 	glPopMatrix();
-	
 	glEnd();
 	glFlush();
-	
-
-	
+	glutSwapBuffers();
 	glutPostRedisplay();
 
 
@@ -111,11 +77,15 @@ void displayTeapot()
 void reshape(int w, int h) //functie redesenare
 {
 	glViewport(0, 0, (GLsizei)w, (GLsizei)h);//stabiliere viewport la dimensiunea ferestrei
+	glMatrixMode(GL_MODELVIEW); //specificare matrice modificabila la valoare argumentului de modificare
+	glLoadIdentity();//initializare sistem coordonate
 	glMatrixMode(GL_PROJECTION); //specificare matrice modificabila la valoare argumentului de modificare
 	glLoadIdentity();//initializare sistem coordonate
 	gluOrtho2D(0.0, (GLdouble)w, 0.0, (GLdouble)h); //stabileste volumul de vedere folosind o proiectie otrografica
-
+	
 }
+
+
 
 void meniu_principal(int key)
 {
@@ -134,17 +104,13 @@ void submenu_3d(int key)
 		case 0:
 			printf("Torus\n");
 			displayTorus();
-			//glClearColor(0.0, 0.0, 0.0, 0.0);
-			glutDisplayFunc(displayTorus);
-			//glutIdleFunc(idle);
-			glutReshapeFunc(reshape);
-
+			glutDisplayFunc(displayTorus);//stabileste functia de callback pentru fereastra
+			glutReshapeFunc(reshape);//se cheama daca se schimba dimensiunea ferestrei
 			break;
 		case 1:
 			printf("Ceainic\n");
 			glutDisplayFunc(displayTeapot);
 			glutReshapeFunc(reshape);
-
 			break;
 
 	}
@@ -171,22 +137,17 @@ void submenu_2d(int key)
 	}
 }
 
-void idle()
-{
-	yrotate += 1;
-	displayTorus;
-}
 
 int main(int argc, char** argv)
 {
 	
 
 	glutInit(&argc, argv);
-	glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
-	glutInitWindowSize(400, 400);
+	glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB);
+	glutInitWindowSize(600, 600);
 	glutInitWindowPosition(150, 150);
 	glutCreateWindow("Exemplul 1");// functie creare fereastra
-	init();
+	
 	//Meniuri
 	submeniu3D = glutCreateMenu(submenu_3d);
 	glutAddMenuEntry("Deseneaza Torus", 0);
@@ -197,19 +158,12 @@ int main(int argc, char** argv)
 	glutAddMenuEntry("Dreptunghi", 1);
 
 	meniu_main = glutCreateMenu(meniu_principal);//creeaza meniul
-	glutAddSubMenu("Obiecte 3D", submeniu3D);
+	glutAddSubMenu("Obiecte 3D", submeniu3D);//se creaza submeniul si se apeleaza
 	glutAddSubMenu("Obiected 2D", submeniu2D);
 	glutAddMenuEntry("Exit", 0);
-	glutAttachMenu(GLUT_RIGHT_BUTTON);
+	glutAttachMenu(GLUT_RIGHT_BUTTON);//se ataseaza intrarea in meniul principal de butonul drept al mouse-ului
 
-	//
-
-	
-
-	
-	
-
-	glutMainLoop();
+	glutMainLoop();//functia principala de executare a comenzilor opengl
 	return 0;
 
 }
